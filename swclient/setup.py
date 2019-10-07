09fae4f1f223515ebe838c2343214cb948f6bc22
sean meng <sean.meng@lairdtech.com>
2019-10-06 20:12:55 -0400

    Wrap swupdate client for Python
    Bug 13442

---
+++ swclient/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup, Extension
+
+swclient_module = Extension('swclient',
+							include_dirs = ['./'],
+							libraries_dirs = [''],
+							libraries = ['swupdate-client'],
+							sources = ['swclient.c'])
+
+setup(name='swclient',
+	version='1.0.0',
+	description='swupdate client module written in C',
+	ext_modules=[swclient_module])
